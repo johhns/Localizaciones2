149ca86585e5aecac4a78decafab32252e53df7b
johhns <johhns@hotmail.com>
2021-10-05 20:01:26 -0600

    v 1.01

--- app/src/main/java/com/developer/johhns/localizacion/MainActivity.java
+++ app/src/main/java/com/developer/johhns/localizacion/MainActivity.java
@@ -2,11 +2,15 @@ package com.developer.johhns.localizacion;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -21,7 +25,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     static final String[] E = { "fuera de servicio",
                                 "temporalmente no disponible",
                                 "dispobible" } ;
-    LocationManager manejadorLoc ;
+    LocationManager manejador ;
     String          proveedor ;
     TextView        salida ;
 
@@ -31,51 +35,88 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         setContentView(R.layout.activity_main);
 
         salida = findViewById( R.id.salida ) ;
-        manejadorLoc = (LocationManager) getSystemService( LOCATION_SERVICE ) ;
+        manejador = (LocationManager) getSystemService( LOCATION_SERVICE ) ;
         muestraProveedores();
         Criteria criterio = new Criteria();
         criterio.setCostAllowed(false);
         criterio.setAltitudeRequired(false);
         criterio.setAccuracy(Criteria.ACCURACY_FINE);
-        proveedor = manejadorLoc.getBestProvider(criterio,true) ;
-        Location localizacion = manejadorLoc.getLastKnownLocation(proveedor);
-        muestraLocaliz(localizacion);
+        proveedor = manejador.getBestProvider(criterio,true) ;
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_CALL_LOG) == PackageManager.PERMISSION_GRANTED) {
+            Location localizacion = manejador.getLastKnownLocation(proveedor);
+            muestraLocaliz(localizacion);
+        }
+    }
+
+    @Override protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_CALL_LOG) == PackageManager.PERMISSION_GRANTED) {
+            manejador.requestLocationUpdates(proveedor, TIEMPO_MIN, DISTANCIA_MIN,
+                    this);
+        }
+    }
+
+    @Override protected void onPause() {
+        super.onPause();
+        manejador.removeUpdates(this);
+    }
+
+    // Métodos de la interfaz LocationListener
+    @Override public void onLocationChanged(Location location) {
+        log("Nueva localización: ");
+        muestraLocaliz(location);
+    }
+
+    @Override public void onProviderDisabled(String proveedor) {
+        log("Proveedor deshabilitado: " + proveedor + "\n");
+    }
+
+    @Override public void onProviderEnabled(String proveedor) {
+        log("Proveedor habilitado: " + proveedor + "\n");
+    }
+
+    @Override public void onStatusChanged(String proveedor, int estado,
+                                          Bundle extras) {
+        log("Cambia estado proveedor: " + proveedor + ", estado="
+                + E[Math.max(0, estado)] + ", extras=" + extras + "\n");
+    }
+
+    // Métodos para mostrar información
+    private void log(String cadena) {
+        salida.append(cadena + "\n");
     }
 
     private void muestraLocaliz(Location localizacion) {
+        if (localizacion == null)
+            log("Localización desconocida\n");
+        else
+            log(localizacion.toString() + "\n");
     }
 
     private void muestraProveedores() {
-        
+        log("Proveedores de localización: \n ");
+        List<String> proveedores = manejador.getAllProviders();
+        for (String proveedor : proveedores) {
+            muestraProveedor(proveedor);
+        }
     }
 
-    @Override
-    public void onLocationChanged(@NonNull Location location) {
-
+    private void muestraProveedor(String proveedor) {
+        LocationProvider info = manejador.getProvider(proveedor);
+        log("LocationProvider[ " + "getName=" + info.getName()
+                + ", isProviderEnabled="
+                + manejador.isProviderEnabled(proveedor) + ", getAccuracy="
+                + A[Math.max(0, info.getAccuracy())] + ", getPowerRequirement="
+                + P[Math.max(0, info.getPowerRequirement())]
+                + ", hasMonetaryCost=" + info.hasMonetaryCost()
+                + ", requiresCell=" + info.requiresCell()
+                + ", requiresNetwork=" + info.requiresNetwork()
+                + ", requiresSatellite=" + info.requiresSatellite()
+                + ", supportsAltitude=" + info.supportsAltitude()
+                + ", supportsBearing=" + info.supportsBearing()
+                + ", supportsSpeed=" + info.supportsSpeed() + " ]\n");
     }
 
-    @Override
-    public void onLocationChanged(@NonNull List<Location> locations) {
-
-    }
-
-    @Override
-    public void onFlushComplete(int requestCode) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(@NonNull String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(@NonNull String provider) {
-
-    }
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
-    }
 }
